6792faabb855297a9a53bde2b8a7380361ea7032
liledix4 <liledix4@musician.org>
2024-11-23 14:59:59 +0000

    gjngjksgjkjgk

--- test/xhr.js
+++ test/xhr.js
@@ -8,6 +8,18 @@ document.getElementById('submit0').addEventListener('click', () => {
             url: server
         },
         response => {
+            const json = JSON.parse(response);
+            const statusSelector = document.getElementById('access-allowed');
+            switch (json[0]['Is access allowed?']) {
+                case true:
+                    statusSelector.innerHTML = 'Access is allowed!';
+                    break;
+                case false:
+                    statusSelector.innerHTML = 'Access is NOT allowed!';
+                    break;
+                default:
+                    statusSelector.innerHTML = '???? Try again';
+            }
             document.getElementById('content').innerText = response;
         }
     );
